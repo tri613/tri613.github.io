b68fb60b5d780f7ea8113ce1cf7fb98114f032e3
tri613 <tri613@gmail.com>
2017-06-20 13:27:27 +0800

    Site updated: 2017-06-20 13:27:26

--- js/main.js
+++ js/main.js
@@ -1,49 +1,63 @@
-if (!!$.prototype.justifiedGallery) {  // if justifiedGallery method is defined
-  var options = {
-    rowHeight: 140,
-    margins: 4,
-    lastRow: 'justify'
-  };
-  $('.article-gallery').justifiedGallery(options);
-}
+(function($){
+    var toTop = ($('#sidebar').height() - $(window).height()) + 60;
+    // Caption
+    $('.article-entry').each(function(i) {
+        $(this).find('img').each(function() {
+            if (this.alt && !(!!$.prototype.justifiedGallery && $(this).parent('.justified-gallery').length)) {
+                $(this).after('<span class="caption">' + this.alt + '</span>');
+            }
 
-$(document).ready(function(){
-  $("#menu-icon").click(function(){
-    $('#menu').toggleClass('open');
-    $('#menu-icon').toggleClass('active');
-    // if ( $('#menu').css('display') == 'hidden' ) {
-    //   $('#menu').css('visibility','visible');
-    //   $('#menu-icon').addClass('active');
+            // 对于已经包含在链接内的图片不适用lightGallery
+            if ($(this).parent().prop("tagName") !== 'A') {
+                $(this).wrap('<a href="' + this.src + '" title="' + this.alt + '" class="gallery-item"></a>');
+            }
+        });
+    });
+    if (typeof lightGallery != 'undefined') {
+        var options = {
+            selector: '.gallery-item',
+        };
+        $('.article-entry').each(function(i, entry) {
+            lightGallery(entry, options);
+        });
+        lightGallery($('.article-gallery')[0], options);
+    }
+    if (!!$.prototype.justifiedGallery) {  // if justifiedGallery method is defined
+        var options = {
+            rowHeight: 140,
+            margins: 4,
+            lastRow: 'justify'
+        };
+        $('.justified-gallery').justifiedGallery(options);
+    }
 
-    //   var topDistance = $("#nav").offset().top;
+    // Profile card
+    $(document).on('click', function () {
+        $('#profile').removeClass('card');
+    }).on('click', '#profile-anchor', function (e) {
+        e.stopPropagation();
+        $('#profile').toggleClass('card');
+    }).on('click', '.profile-inner', function (e) {
+        e.stopPropagation();
+    });
 
-    //   if ( $('#menu').css('visibility') != 'hidden' && topDistance < 50 ) {
-    //     $("#menu > #nav").show();
-    //   } else if ($('#menu').css('visibility') != 'hidden' && topDistance > 100) {
-    //     $("#menu > #nav").hide();
-    //   }
-    //   return false;
-    // } else {
-    //   $('#menu').css('visibility','hidden');
-    //   $('#menu-icon').removeClass('active');
-    //   return false;
-    // }
-  });
+    // To Top
+    if ($('#sidebar').length) {
+        $(document).on('scroll', function () {
+            if ($(document).width() >= 800) {
+                if(($(this).scrollTop() > toTop) && ($(this).scrollTop() > 0)) {
+                    $('#toTop').fadeIn();
+                    $('#toTop').css('left', $('#sidebar').offset().left);
+                } else {
+                    $('#toTop').fadeOut();
+                }
+            } else {
+                $('#toTop').fadeIn();
+                $('#toTop').css('right', 20);
+            }
+        }).on('click', '#toTop', function () {
+            $('body, html').animate({ scrollTop: 0 }, 600);
+        });
+    }
 
-    /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-  $("#header > #nav > ul > .icon").click(function() {
-    $("#header > #nav > ul").toggleClass("responsive");
-  });
-
-  // if ( $( "#menu" ).length ) {
-  //   $(window).on('scroll', function() {
-  //     var topDistance = $("#menu > #nav").offset().top;
-
-  //     if ( $('#menu').css('visibility') != 'hidden' && topDistance < 50 ) {
-  //       $("#menu > #nav").show();
-  //     } else if ($('#menu').css('visibility') != 'hidden' && topDistance > 100) {
-  //       $("#menu > #nav").hide();
-  //     }
-  //   });
-  // }
-});
+})(jQuery);
